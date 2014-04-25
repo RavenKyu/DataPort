d02374b6bdf7abe90b7a8cbce96df894875a3a3b
Duk Kyu Lim <hong18s@gmail.com>
2014-04-25 21:09:05 +0900

    기본적인 디자인 완성과 실행이 가능 * UI 파일 생성 및 pyuic4 를 이용하여 파이썬 파일로 컨버팅 * 윈도우를 실행 할 수 있는 기본 코드 작성

---
+++ src/main.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+import sys, time
+from PyQt4 import QtGui, QtCore
+from ui import Ui_MainWindow
+
+class mainForm(QtGui.QMainWindow):
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+  
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    myapp = mainForm()
+    myapp.show()
+    sys.exit(app.exec_())
+ 
+ 
